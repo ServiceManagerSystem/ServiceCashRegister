493f7dda23c422e7fe1b6c5bb3eac96b7c5b80ab
Georgi Iliev <neokaozzz@gmail.com>
2016-04-29 23:01:23 +0300

    Restructured documents tab VM, refactors coming soon.

--- CashRegisterRepairs/ViewModel/TemplatesDocumentsViewModel.cs
+++ CashRegisterRepairs/ViewModel/TemplatesDocumentsViewModel.cs
@@ -13,7 +13,14 @@ namespace CashRegisterRepairs.ViewModel
 {
     public class TemplatesDocumentsViewModel : INotifyPropertyChanged, IViewModel
     {
+        // Subview disable/enable control field
+        private bool canOpenSubviewForm = true;
+
+        // Command disable/enable control field
         private bool canExecute = true;
+
+        // Notify bullshit
+        #region NOTIFY
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged([CallerMemberName] String propName = "")
         {
@@ -22,9 +29,13 @@ namespace CashRegisterRepairs.ViewModel
                 PropertyChanged(this, new PropertyChangedEventArgs(propName));
             }
         }
+        #endregion
 
+        // DB Context
         private readonly CashRegisterServiceContext dbModel = new CashRegisterServiceContext();
 
+        // Grid collections
+        #region Grid Collections
         private ObservableCollection<Template> _templates;
         public ObservableCollection<Template> Templates
         {
@@ -38,7 +49,9 @@ namespace CashRegisterRepairs.ViewModel
             get { return _documents; }
             set { _documents = value; }
         }
+        #endregion
 
+        // CTOR(NEEDS REFACTOR)
         public TemplatesDocumentsViewModel()
         {
             // Initialize backing datagrid collections
@@ -67,9 +80,19 @@ namespace CashRegisterRepairs.ViewModel
             dbModel.Devices.ToList().ForEach(device => Devices.Add(device.DeviceModel.DEVICE_NUM_PREFIX + device.DEVICE_NUM_POSTFIX));
 
             // Initialize commands
+            EnableSubviewDisplay = new TemplateCommand(EnableSubview, param => this.canExecute);
             DisplayDocumentsCommand = new TemplateCommand(ShowDocumentsOfTemplate, param => this.canExecute);
             AddTemplateCommand = new TemplateCommand(ShowTemplateAdditionForm, param => this.canExecute);
             AddDocumentCommand = new TemplateCommand(ShowDocumentAdditionForm, param => this.canExecute);
+        }
+
+        // COMMANDS
+        #region COMMANDS
+        private ICommand _enableSubviewDisplay;
+        public ICommand EnableSubviewDisplay
+        {
+            get { return _enableSubviewDisplay; }
+            set { _enableSubviewDisplay = value; }
         }
 
         private ICommand _addTemplateCommand;
@@ -92,7 +115,17 @@ namespace CashRegisterRepairs.ViewModel
             get { return _displayDocumentsCommand; }
             set { _displayDocumentsCommand = value; }
         }
+        #endregion
 
+        // METHODS
+        #region METHODS - ALL
+        private void EnableSubview(object obj)
+        {
+            canOpenSubviewForm = true;
+        }
+
+        // SHOW FORM METHODS
+        #region SHOW FORM METHODS (NEEDS REFACTOR)
         private void ShowDocumentsOfTemplate(object obj)
         {
             throw new NotImplementedException();
@@ -100,11 +133,12 @@ namespace CashRegisterRepairs.ViewModel
 
         private void ShowTemplateAdditionForm(object obj)
         {
-            if (TransitionContext.CanOpenSubview())
+            if (canOpenSubviewForm)
             {
                 AddTemplateView addTemplatesView = new AddTemplateView();
+                addTemplatesView.DataContext = obj;
                 addTemplatesView.Show();
-                TransitionContext.DisableSubviewOpen();
+                canOpenSubviewForm = false;
             }
         }
 
@@ -120,29 +154,17 @@ namespace CashRegisterRepairs.ViewModel
             SelectedDevice = IsDeviceAuto ? TransitionContext.selectedDevice : null;
 
             // Move on to view ( allow one instance at a time )
-            if (TransitionContext.CanOpenSubview())
+            if (canOpenSubviewForm)
             {
                 AddDocumentView addDocumentsView = new AddDocumentView();
                 addDocumentsView.Show();
-                TransitionContext.DisableSubviewOpen();
+                canOpenSubviewForm = false;
             }
         }
+        #endregion
 
-        private object _selectedTemplateFromGrid;
-        public object SelectedTemplate
-        {
-            get { return _selectedTemplateFromGrid; }
-            set { _selectedTemplateFromGrid = value; NotifyPropertyChanged(); }
-        }
-
-        private object _selectedDocumentFromGrid;
-        public object SelectedDocument
-        {
-            get { return _selectedDocumentFromGrid; }
-            set { _selectedDocumentFromGrid = value; NotifyPropertyChanged(); }
-        }
-
-        // NEEDS A LOT MORE PROPERTIES SEE VIEW`s BINDINGS...
+        // ComboBox filler collections(maybe make them fields - see other tabs)
+        #region ComboBox FILLERS
         private List<string> _sitesFromDB;
         public List<string> Sites
         {
@@ -163,7 +185,48 @@ namespace CashRegisterRepairs.ViewModel
             get { return _devicesFromDB; }
             set { _devicesFromDB = value; NotifyPropertyChanged(); }
         }
+        #endregion
 
+        // SELECTION PROPS
+        #region SELECTION PROPS
+        private Client _selectedClient;
+        public Client SelectedClient
+        {
+            get { return _selectedClient; }
+            set { _selectedClient = value; NotifyPropertyChanged(); }
+        }
+
+        private Site _selectedSite;
+        public Site SelectedSite
+        {
+            get { return _selectedSite; }
+            set { _selectedSite = value; NotifyPropertyChanged(); }
+        }
+
+        private Device _selectedDevice;
+        public Device SelectedDevice
+        {
+            get { return _selectedDevice; }
+            set { _selectedDevice = value; NotifyPropertyChanged(); }
+        }
+
+        private object _selectedTemplateFromGrid;
+        public object SelectedTemplate
+        {
+            get { return _selectedTemplateFromGrid; }
+            set { _selectedTemplateFromGrid = value; NotifyPropertyChanged(); }
+        }
+
+        private object _selectedDocumentFromGrid;
+        public object SelectedDocument
+        {
+            get { return _selectedDocumentFromGrid; }
+            set { _selectedDocumentFromGrid = value; NotifyPropertyChanged(); }
+        }
+        #endregion
+
+        // PROPS - NEEDS A LOT MORE PROPERTIES SEE VIEW`s BINDINGS...
+        #region PROPS
         private bool _isClientAuto;
         public bool IsClientAuto
         {
@@ -184,28 +247,8 @@ namespace CashRegisterRepairs.ViewModel
             get { return _isDeviceAuto; }
             set { _isDeviceAuto = value; NotifyPropertyChanged(); }
         }
-
-        private Client _selectedClient;
-        public Client SelectedClient
-        {
-            get { return _selectedClient; }
-            set { _selectedClient = value; NotifyPropertyChanged(); }
-        }
-
-        private Site _selectedSite;
-        public Site SelectedSite
-        {
-            get { return _selectedSite; }
-            set { _selectedSite = value; NotifyPropertyChanged(); }
-        }
-
-        private Device _selectedDevice;
-        public Device SelectedDevice
-        {
-            get { return _selectedDevice; }
-            set { _selectedDevice = value; }
-        }
-
+        #endregion
+        #endregion
     }
 
 }
