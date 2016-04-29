c41b3d638a223af6f890afcf1aee15c4a252159a
Georgi Iliev <neokaozzz@gmail.com>
2016-04-30 00:13:19 +0300

    Styles applied to all adition views.

--- CashRegisterRepairs/Utilities/TransitionContext.cs
+++ CashRegisterRepairs/Utilities/TransitionContext.cs
@@ -1,38 +1,13 @@
 ﻿using CashRegisterRepairs.Model;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace CashRegisterRepairs.Utilities
 {
     public static class TransitionContext
     {
-        public static int selectedClientIndex;
+        // TO BE REMOVED ALONG WITH THE CLASS ITSELF
         public static Site selectedSite;
         public static Device selectedDevice;
-        public static DeviceModel selectedModel;
-
-        private static bool canOpenSubview;
-
-        public static void DisableSubviewOpen()
-        {
-            canOpenSubview = true;
-        }
-
-        public static void EnableSubviewOpen()
-        {
-            canOpenSubview = false;
-        }
-
-        public static bool CanOpenSubview()
-        {
-            return (!canOpenSubview);
-        }
-
-        // Use to avoid object stuck in cache
-        public static void ConsumeObjectsAfterUse(params object[] usedObjects)
-        {
-            usedObjects.ToList().ForEach(usedObject => usedObject = null);
-        }
 
     }
 
