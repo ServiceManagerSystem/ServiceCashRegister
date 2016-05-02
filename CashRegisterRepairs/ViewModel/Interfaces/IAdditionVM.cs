0a149b078d4bbfb805610c4170183318104d3c23
Georgi Iliev <neokaozzz@gmail.com>
2016-05-02 16:29:59 +0300

    Removed unnecessary classes/interfaces

--- CashRegisterRepairs/ViewModel/Interfaces/IAdditionVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CashRegisterRepairs.ViewModel.Interfaces
-{
-    interface IAdditionVM:IViewModel
-    {
-        void ClearFields();
-        void SaveRecord(object obj);
-        void CommitRecords(object obj);
-    }
-}
