f29968597cbf27a9e8166105340848234066f610
Georgi Iliev <neokaozzz@gmail.com>
2016-05-04 14:21:32 +0300

    Recommit

---
+++ CashRegisterRepairs/Utilities/AuthenticationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace CashRegisterRepairs.Utilities
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public bool Login(string username, SecureString password)
+        {
+            IntPtr passwordBSTR = default(IntPtr);
+            string insecurePassword = "";
+            try
+            {
+                passwordBSTR = Marshal.SecureStringToBSTR(password);
+                insecurePassword = Marshal.PtrToStringBSTR(passwordBSTR);
+            }
+            catch
+            {
+                insecurePassword = "";
+            }
+            return MockServiceProxyCall(username, insecurePassword);
+        }
+
+        private bool MockServiceProxyCall(string username, string password)
+        {
+            if (username == "Rosen" && password == "1234") return true;
+            else return false;
+        }
+    }
+}
--- CashRegisterRepairs/ViewModel/LoginViewModel.cs
+++ CashRegisterRepairs/ViewModel/LoginViewModel.cs
@@ -4,7 +4,8 @@ using System.Windows.Input;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using CashRegisterRepairs.View;
-using CashRegisterRepairs.Config;
+using System.Security;
+using CashRegisterRepairs.Utilities;
 
 namespace CashRegisterRepairs.ViewModel
 {
