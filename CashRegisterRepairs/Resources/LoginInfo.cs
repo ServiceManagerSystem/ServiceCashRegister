3e98a250aa953e36d127a1cb4746af2c6430ba8c
Georgi Iliev <neokaozzz@gmail.com>
2016-05-04 14:22:33 +0300

    Removed unnecessary class

--- CashRegisterRepairs/Resources/LoginInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿ namespace CashRegisterRepairs.Config
-{
-    public static class LoginInfo
-    {
-        public static readonly string username = "Rosen";
-        public static readonly string password = "12340";
-    }
-}
