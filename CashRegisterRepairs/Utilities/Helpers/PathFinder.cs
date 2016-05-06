ca41723d1708536693f482ecd6d566b1c630769e
Georgi Iliev <neokaozzz@gmail.com>
2016-05-06 13:43:35 +0300

    PathFinder recommit

---
+++ CashRegisterRepairs/Utilities/Helpers/PathFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace CashRegisterRepairs.Utilities.Helpers
+{
+    public static class PathFinder
+    {
+        public static readonly string serviceProfilePath = ResolveAppPath() + @"\Resources\Service.txt";
+        public static readonly string temporaryDocumentsPath = ResolveAppPath() + @"\Resources\TemporaryDocuments\";
+
+        public static readonly string contractWordTemplatePath = ResolveAppPath() + @"\Resources\WordTemplates\ContractTemplate.dotx";
+        public static readonly string certificateWordTemplatePath = ResolveAppPath() + @"\Resources\WordTemplates\CertificateTemplate.dotx";
+        public static readonly string protocolWordTemplatePath = ResolveAppPath() + @"\Resources\WordTemplates\ProtocolTemplate.dotx";
+
+        public static string[] FetchServiceProfile()
+        {
+            return File.ReadAllText(serviceProfilePath).Split('\n');
+        }
+
+        public static string FetchTemporaryWordDocumentFullPath()
+        {
+           return temporaryDocumentsPath + Guid.NewGuid() + ".doc";
+        }
+
+        public static string FetchWordTemplatePath(string templateType)
+        {
+            switch (templateType)
+            {
+                case "Договор":
+                    return contractWordTemplatePath;
+                case "Свидетелство":
+                    return certificateWordTemplatePath;
+                case "Протокол":
+                    return protocolWordTemplatePath;
+                default:
+                    return null;
+            }
+        }
+
+        private static string ResolveAppPath()
+        {
+            string assemblyPathNode = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string binPathNode = Directory.GetParent(assemblyPathNode).FullName;
+            return Directory.GetParent(binPathNode).FullName;
+        }
+
+    }
+}
